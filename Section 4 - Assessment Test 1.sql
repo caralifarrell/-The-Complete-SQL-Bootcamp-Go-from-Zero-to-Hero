1692c420cd60be1be6417b88c9ab6656832138a6
Cara-Li Farrell <79096101+caralifarrell@users.noreply.github.com>
2023-07-12 22:45:56 -0400

    Update Section 4 - Assessment Test 1.sql

--- Section 4 - Assessment Test 1.sql
+++ Section 4 - Assessment Test 1.sql
@@ -1,6 +1,6 @@
 /* Section 4 - Assessment Test 1 */
 
-/*1. Return the customer IDs of customers who have spent at least $110 with the staff member who has an ID of 2.
+/* 1. Return the customer IDs of customers who have spent at least $110 with the staff member who has an ID of 2.
 The answer should be customers 187 and 148. */
 SELECT customer_id, SUM(amount) FROM payment
 WHERE staff_id=2
